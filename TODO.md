e9829a590ab993a2834ed7bf8e4fbeb36e724506
Danny McClanahan <danieldmcclanahan@gmail.com>
2015-05-16 16:32:10 -0700

    BROKEN: refactored classes and overall goals

--- TODO.md
+++ TODO.md
@@ -1,7 +1,7 @@
 (in topological order)
-1. do this with c
-2. add macros
-3. extend to multiple files (don't forget about static!)
-4. make preprocessor macros work (be able to read their expansion as well)
+1. do types and variables in c with multiple files (don't forget about static!)
+2. make preprocessor macros work (be able to read their expansion as well)
+3. add struct/class member decls/refs
+4. add aliases (typedefs and pointer/reference)
 5. extend to c++ (templates, namespaces, virtual functions!)
 6. remove requirement to include clang headers, or at least automate it
--- cursor.h
+++ cursor.h
@@ -2,20 +2,41 @@
 #define CURSOR_H
 
 // std includes
-#include <list>
 #include <stdexcept>
-#include <unordered_map>
-#include <functional>
 // external includes
 #include <clang-c/Index.h>
 
 namespace semantic_code_browser {
 
+/*
+
+Cursor <- EntityCursor<Type>  <- AliasableCursor<Type> <- (Decl|Ref)Cursor<Type>
+       <- EntityCursor<Value> <- (Decl|Ref)Cursor<Value>
+       <- AliasCursor<Type>
+
+DeclCursor<Type|Value> <- CanonicalDeclCursor<Type|Value>
+                       <- DefinitionDeclCursor<Type|Value>
+
+ */
+
+enum Specifier { Type, Value };
+
 class Cursor;
+
+template <Specifier S>
+class EntityCursor;
+
+template <Specifier S>
 class DeclCursor;
+template <Specifier S>
 class RefCursor;
+
+template <Specifer S>
 class CanonicalDeclCursor;
+template <Specifier S>
 class DefinitionDeclCursor;
+
+template <Specifier S>
 class AliasCursor;
 
 // root of inheritance hierarchy
@@ -24,18 +45,7 @@ class Cursor {
   const CXCursor cur;
   const CXTranslationUnit & tu;
 
-  const CanonicalDeclCursor * canonical;
-  const DefinitionDeclCursor * defn;
-  std::list<const DeclCursor *> decls;
-  std::list<const RefCursor *> refs;
-
-  static std::unordered_map<const std::string, const CanonicalDeclCursor *>
-   nodes;
-
   Cursor(const CXCursor, const CXTranslationUnit &);
-
-  const CanonicalDeclCursor * findCanonical();
-  const DefinitionDeclCursor * findDefinition();
 
   // CXCursorKind corresponding to each top-level Cursor subclass
   // declarations of variables, types, functions, fields, labels
@@ -59,42 +69,64 @@ class Cursor {
   };
 
   static const Cursor * MakeCursor(const CXCursor, const CXTranslationUnit &);
-  static void ForEach(std::function<void(const std::string, const Cursor *) >);
+  static void
+   ForEachEntity(std::function<void(const std::string, const Cursor *) >);
   static void FreeAll();
 
   bool operator==(const Cursor &) const;
   bool operator!=(const Cursor &) const;
+};
 
+// level 1 of inheritance hierarchy
+template <Specifier S>
+class EntityCursor : public Cursor {
+ private:
+  const CanonicalDeclCursor * canonical;
+  const DefinitionDeclCursor * defn;
+  std::list<const DeclCursor *> decls;
+  std::list<const RefCursor *> refs;
+
+  static std::unordered_map<const std::string, const CanonicalDeclCursor *>
+   nodes;
+
+ public:
   std::list<const DeclCursor *> getDecls() const;
   const CanonicalDeclCursor * getCanonical() const;
   const DefinitionDeclCursor * getDefinition() const;
   std::list<const RefCursor *> getRefs() const;
 };
 
-// level 1 of inheritance hierarchy
-class DeclCursor : public Cursor {
- protected:
-  DeclCursor(CXCursor, CXTranslationUnit &);
-};
-
-class RefCursor : public Cursor {
- protected:
-  RefCursor(CXCursor, CXTranslationUnit &);
-};
-
+template <Specifier S>
 class AliasCursor : public Cursor {
+  static_assert(S == Type, "Alias only has meaning for types.");
+
  protected:
   AliasCursor(CXCursor, CXTranslationUnit &);
   std::pair<Cursor *, Cursor *> typeAlias;
 };
 
 // level 2 of inheritance hierarchy
-class CanonicalDeclCursor : public DeclCursor {
+template <Specifier S>
+class DeclCursor : public EntityCursor<S> {
+ protected:
+  DeclCursor(CXCursor, CXTranslationUnit &);
+};
+
+template <Specifier S>
+class RefCursor : public EntityCursor<S> {
+ protected:
+  RefCursor(CXCursor, CXTranslationUnit &);
+};
+
+// level 3 of inheritance hierarchy
+template <Specifier S>
+class CanonicalDeclCursor : public DeclCursor<S> {
  protected:
   CanonicalDeclCursor(CXCursor, CXTranslationUnit &);
 }
 
-class DefinitionDeclCursor : public DeclCursor {
+template <Specifier S>
+class DefinitionDeclCursor : public DeclCursor<S> {
  protected:
   CanonicalDeclCursor(CXCursor, CXTranslationUnit &);
 }
