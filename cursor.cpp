9abaa8dbf72340205aab2aab3fe21fb9ae34cd5c
Danny McClanahan <danieldmcclanahan@gmail.com>
2015-05-19 21:55:37 -0700

    A-OK: doesn't do anything, but cursor class down

--- cursor.cpp
+++ cursor.cpp
@@ -1,6 +1,7 @@
 #include "cursor.hpp"
 
 namespace semantic_code_browser {
+/* cursor types */
 const CXCursorKind Cursor::TypeDeclCursorKinds[] = {
  CXCursor_StructDecl, CXCursor_UnionDecl, CXCursor_EnumDecl};
 const CXCursorKind Cursor::ValDeclCursorKinds[] = {
@@ -10,6 +11,18 @@ const CXCursorKind Cursor::TypeRefCursorKinds[] = {CXCursor_TypeRef};
 const CXCursorKind Cursor::ValRefCursorKinds[] = {CXCursor_VariableRef,
                                                   CXCursor_DeclRefExpr};
 
+/* static helper methods */
+std::string Cursor::GetStringFromCXString(CXString cxs) {
+  std::string ret(clang_getCString(cxs));
+  clang_disposeString(cxs);
+  return ret;
+}
+
+/* pointless virtual dtor */
+Cursor::~Cursor() {
+}
+
+/* entry point to cursor class */
 Cursor * Cursor::MakeCursor(CXCursor c, CXTranslationUnit & tu) {
   for (auto & typeDeclCursorKind : TypeDeclCursorKinds) {
     if (typeDeclCursorKind == clang_getCursorKind(c)) {
@@ -35,12 +48,40 @@ Cursor * Cursor::MakeCursor(CXCursor c, CXTranslationUnit & tu) {
   return nullptr;
 }
 
-Cursor::Cursor(CXCursor c, CXTranslationUnit & tu) : mCursor(c), mTU(tu) {
+/* constructor */
+Cursor::Cursor(CXCursor c, CXTranslationUnit & tu)
+   : mCursor(c),
+     mBegin(clang_getRangeStart(clang_getCursorExtent(c))),
+     mEnd(clang_getRangeEnd(clang_getCursorExtent(c))),
+     mTU(tu),
+     mName(Cursor::GetStringFromCXString(clang_getCursorSpelling(c))) {
 }
 
+/* simple accessors */
+const CXCursor & Cursor::get() {
+  return mCursor;
+}
+
+const CXTranslationUnit & Cursor::getTranslationUnit() {
+  return mTU;
+}
+
+const CXSourceLocation & Cursor::getBegin() {
+  return mBegin;
+}
+
+const CXSourceLocation & Cursor::getEnd() {
+  return mEnd;
+}
+
+/* ctors/dtors for derived classes */
 template <Specifier S>
 EntityCursor<S>::EntityCursor(CXCursor c, CXTranslationUnit & tu)
    : Cursor(c, tu) {
+}
+
+template <Specifier S>
+EntityCursor<S>::~EntityCursor() {
 }
 
 template <Specifier S>
@@ -49,7 +90,15 @@ DeclCursor<S>::DeclCursor(CXCursor c, CXTranslationUnit & tu)
 }
 
 template <Specifier S>
+DeclCursor<S>::~DeclCursor() {
+}
+
+template <Specifier S>
 RefCursor<S>::RefCursor(CXCursor c, CXTranslationUnit & tu)
    : EntityCursor<S>(c, tu) {
+}
+
+template <Specifier S>
+RefCursor<S>::~RefCursor() {
 }
 }
--- cursor.hpp
+++ cursor.hpp
@@ -2,50 +2,76 @@
 #define CURSOR_H
 
 // std includes
-#include <stdexcept>
+#include <tuple>
 // external includes
 #include <clang-c/Index.h>
 
 namespace semantic_code_browser {
 class Cursor {
  protected:
-  Cursor(CXCursor, CXTranslationUnit &);
-
-  CXCursor mCursor;
-  CXTranslationUnit & mTU;
-
+  /* types of cursors used in multiplexing Cursor derived class in MakeCursor */
   static const CXCursorKind TypeDeclCursorKinds[];
   static const CXCursorKind ValDeclCursorKinds[];
   static const CXCursorKind TypeRefCursorKinds[];
   static const CXCursorKind ValRefCursorKinds[];
 
- public:
-  static Cursor * MakeCursor(CXCursor, CXTranslationUnit &);
-  // add some pure virtual methods here maybe
+  /* static helper methods */
+  /*
+    THIS DISPOSES OF THE CXSTRING AND WILL CAUSE MEMORY ERRORS IF YOU ATTEMPT
+    TO USE THE CXSTRING AFTER USING IT IN THIS FUNCTION
+  */
+  static std::string GetStringFromCXString(CXString);
 
-  CXCursor & get();
-  CXTranslationUnit & getTranslationUnit();
+  /* constructor and members */
+  Cursor(CXCursor, CXTranslationUnit &);
+
+  const CXCursor mCursor;
+  const CXSourceLocation mBegin;
+  const CXSourceLocation mEnd;
+  const CXTranslationUnit & mTU;
+  const std::string mName;
+
+ public:
+  virtual ~Cursor();
+
+  /* entry point to cursor class */
+  static Cursor * MakeCursor(CXCursor, CXTranslationUnit &);
+
+  /* simple accessors */
+  const CXCursor & get();
+  const CXTranslationUnit & getTranslationUnit();
+  const CXSourceLocation & getBegin();
+  const CXSourceLocation & getEnd();
+  const std::string & getName();
 };
 
+/* cursor variants specification */
+// this doesn't matter now, but it will when we introduce aliases, and
+// especially when we introduce namespaces
 enum Specifier { Type, Value };
 
+/* inheritance hierarchy */
 template <Specifier S>
 class EntityCursor : public Cursor {
  protected:
   EntityCursor(CXCursor, CXTranslationUnit &);
-  // add some pure virtual methods here maybe
+
+ public:
+  virtual ~EntityCursor();
 };
 
 template <Specifier S>
 class DeclCursor : public EntityCursor<S> {
  public:
   DeclCursor(CXCursor, CXTranslationUnit &);
+  ~DeclCursor();
 };
 
 template <Specifier S>
 class RefCursor : public EntityCursor<S> {
  public:
   RefCursor(CXCursor, CXTranslationUnit &);
+  ~RefCursor();
 };
 }
 #endif /* CURSOR_H */
